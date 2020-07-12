e122b664296d90d14d28c10599c4377ce2804bfa
Vsumskiy <im.vasia13@gmail.com>
2020-07-12 15:47:54 +0300

    resolve conficts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { FavoriteCardComponent } from './favorite-card/favorite-card.component';
+import { FavoriteCardComponent } from './components/favorite-card/favorite-card.component';
 
 @NgModule({
   declarations: [FavoriteCardComponent, DiffDatePipe],
